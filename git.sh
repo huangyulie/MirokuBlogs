4dfa385a543b4ab3ab43c044cee85ed710dd09e0
huangyulie <1279072972@qq.com>
2023-07-22 11:55:27 +0800

    2023.07.22:改变git.sh

--- git.sh
+++ git.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# author:tianmengmeng
-read -p "请输入本次提交信息:" message    #获取键盘输入的 a 变量数字
+read -p "请输入本次提交信息:" message    #获取键盘输入的message变量数字
 time=$(date "+%Y.%m.%d")
 git add .
 git commit -m ${time}:${message}
